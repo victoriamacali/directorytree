2c99971328a546ac71005fa516ada74d7e3606f1
victoriamacali <72415862+victoriamacali@users.noreply.github.com>
2021-05-02 16:27:54 -0700

    Create NewMain.java

---
+++ NewMain.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+/**
+ *
+ * @author Victoria
+ */
+public class NewMain {
+
+    public static void main(String [] args) throws IOException{
+        java java = new java();
+        java.run();
+
+        
+    }
+    
+}
